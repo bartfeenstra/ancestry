8b47cee6506e615e233721531107d371edf26b05
Bart Feenstra <bart@mynameisbart.com>
2024-01-06 00:00:11 +0000

    Update to Betty 0.3.0b5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ SETUP = {
     'name': 'ancestry',
     'python_requires': '~= 3.11',
     'install_requires': [
-        'betty == 0.3.0b4',
+        'betty == 0.3.0b5',
     ],
     'extras_require': {
         'development': [
