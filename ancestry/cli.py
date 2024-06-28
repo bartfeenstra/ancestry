c5b58fc4ec6d7fe7b352a87d51d555a87cdfcc56
Bart Feenstra <bart@mynameisbart.com>
2024-06-28 18:04:21 +0100

    Upgrade to Betty 0.4

--- ancestry/cli.py
+++ ancestry/cli.py
@@ -1,12 +1,12 @@
 import click
-
-from betty.app import App
-from betty.cli import app_command
+from betty.cli import pass_project, command
+from betty.project import Project
 
 from ancestry.report import report
 
 
 @click.command(help="Generate an ancestry report.")
-@app_command
-async def _report(app: App) -> None:
-    await report(app)
+@pass_project
+@command
+async def _report(project: Project) -> None:
+    await report(project)
